9ea1213ba6022ab14f082da34913d4848c710fcb
saharEslami <seslami2@mail.um.ac.ir>
2021-02-06 02:05:44 +0330

    almost all Queries

---
+++ Database/Query/C/iii/editComment.sql
@@ -0,0 +1,3 @@
+UPDATE Comments
+SET content = 'bad bood'
+WHERE ISBN = "123Y" and ID = 1 and WAccountSpecID = 2;
---
+++ Database/Query/C/iii/userBookComment.sql
@@ -0,0 +1,3 @@
+select content
+from Comments
+where WAccountSpecID = 2 and ISBN = '123Y';

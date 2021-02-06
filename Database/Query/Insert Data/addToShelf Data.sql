5ed3c099f17eb8dc463a817091a01f6cf821a66c
saharEslami <seslami2@mail.um.ac.ir>
2021-02-06 22:17:21 +0330

    data added

---
+++ Database/Query/Insert Data/addToShelf Data.sql
@@ -0,0 +1,13 @@
+
+/*
+second argument define which shelf we want to add, 0 means to read, 1 means reading , and 2 means done 
+*/
+INSERT INTO Connects (ISBN, state , donePages , AccountSpecID )
+VALUES ('123Y' , 0 , 0 , 1 )
+
+
+
+
+
+
+

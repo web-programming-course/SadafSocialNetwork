2e752776f24019384b6003d9ce61bc8262e460fe
kasra korminejad <kasra76@gmail.com>
2021-02-08 03:03:36 +0330

    content added to createComment

--- Database/Query/CreateComment.sql
+++ Database/Query/CreateComment.sql
@@ -3,6 +3,7 @@ CREATE TABLE Comments(
     ID int NOT NULL AUTO_INCREMENT,
     WAccountSpecID int(11),
     likeCount int DEFAULT 0,
+    content text,
     FOREIGN KEY (WAccountSpecID) REFERENCES AccountSpecs(AccountSpecID),
     FOREIGN KEY (ISBN) REFERENCES Books(ISBN),
 	PRIMARY KEY(ISBN , ID)

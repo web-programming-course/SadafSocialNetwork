334415c63ae3ee8cd2824bef4ee880060f9c6ad2
kasra korminejad <kasra76@gmail.com>
2021-02-07 23:25:27 +0330

    pathes in insertBookData.sql updated to work with UploadFile Directory

--- Database/Query/Insert Data/insertBookData.sql
+++ Database/Query/Insert Data/insertBookData.sql
@@ -12,12 +12,12 @@ INSERT INTO `sadaf`.`Books`
 VALUES
 ("111",
 "",
-"/BookReads/asset/books/pdf/1.pdf",
+"./UploadFile/pdf/1.pdf",
 "Alice's Adventures in Wonderland is an 1865 novel by English author Lewis Carroll. It tells of a young girl named Alice, who falls through a rabbit hole into a subterranean fantasy world populated by peculiar, anthropomorphic creatures. It is considered to be one of the best examples of the literary nonsense genre.",
 105,
 "Alice’s Adventures in Wonderland",
 NULL,
-"/BookReads/asset/books/img/1_cover.jpeg",
+"./UploadFile/img/1_cover.jpeg",
 "Lewis Carroll",
 1);
 
@@ -37,12 +37,12 @@ INSERT INTO `sadaf`.`Books`
 VALUES
 ("222",
 "",
-"/BookReads/asset/books/pdf/2.pdf",
+"./UploadFile/pdf/2.pdf",
 "Caligula, formally known as Gaius, was the third Roman emperor, ruling from AD 37 to 41. The son of the popular Roman general Germanicus and Augustus' granddaughter Agrippina the Elder, Caligula was born into the first ruling family of the Roman Empire, conventionally known as the Julio-Claudian dynasty ",
 210,
 "Caligula",
 NULL,
-"/BookReads/asset/books/img/2_cover.jpeg",
+"./UploadFile/img/2_cover.jpeg",
 "Albert Camus",
 1);
 
@@ -62,12 +62,12 @@ INSERT INTO `sadaf`.`Books`
 VALUES
 ("333",
 "",
-"/BookReads/asset/books/pdf/3.pdf",
+"./UploadFile/pdf/3.pdf",
 "The Phantom of the Opera is a 1986 musical with music by Andrew Lloyd Webber, lyrics by Charles Hart, and a libretto by Lloyd Webber and Richard Stilgoe",
 236,
 "The Phantom of the Opera",
 NULL,
-"/BookReads/asset/books/img/3_cover.jpeg",
+"./UploadFile/img/3_cover.jpeg",
 "Andrew Lloyd Webber and Richard Stilgoe",
 1);
 
@@ -87,12 +87,12 @@ INSERT INTO `sadaf`.`Books`
 VALUES
 ("444",
 "",
-"/BookReads/asset/books/pdf/4.pdf",
+"./UploadFile/pdf/4.pdf",
 "Starr Carter, an African-American teenager, faces pressure from various communities and tries to stand up for what is right after she witnesses the shooting of her best friend by the police.",
 23,
 "The Hate U Give",
 NULL,
-"/BookReads/asset/books/img/4_cover.jpeg",
+"./UploadFile/img/4_cover.jpeg",
 "George Tillman Jr.",
 1);
 
@@ -113,12 +113,12 @@ INSERT INTO `sadaf`.`Books`
 VALUES
 ("555",
 "",
-"/BookReads/asset/books/pdf/5.pdf",
+"./UploadFile/pdf/5.pdf",
 "The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951. It was originally intended for adults but is often read by adolescents for its themes of angst, alienation, and as a critique on superficiality in society. It has been translated widely.",
 115,
 "The Catcher in the Rye",
 NULL,
-"/BookReads/asset/books/img/5_cover.jpeg",
+"./UploadFile/img/5_cover.jpeg",
 "J. D. Salinger",
 1);
 
@@ -139,12 +139,12 @@ INSERT INTO `sadaf`.`Books`
 VALUES
 ("666",
 "",
-"/BookReads/asset/books/pdf/6.pdf",
+"./UploadFile/pdf/6.pdf",
 " Pride and Prejudice is an 1813 romantic novel of manners written by Jane Austen. The novel follows the character development of Elizabeth Bennet, the dynamic protagonist of the book who learns about …",
 538,
 "pride and prejudice",
 NULL,
-"/BookReads/asset/books/img/6_cover.jpeg",
+"./UploadFile/img/6_cover.jpeg",
 "Jane Austen",
 1);
 
@@ -165,12 +165,12 @@ INSERT INTO `sadaf`.`Books`
 VALUES
 ("777",
 "",
-"/BookReads/asset/books/pdf/7.pdf",
+"./UploadFile/pdf/7.pdf",
 "The Old Man and the Sea is a short novel written by the American author Ernest Hemingway in 1951 in Cayo Blanco, and published in 1952. It was the last major work of fiction written by Hemingway that was published during his lifetime.",
 20,
 "the old man and the sea",
 NULL,
-"/BookReads/asset/books/img/7_cover.jpeg",
+"./UploadFile/img/7_cover.jpeg",
 "Ernest Hemingway",
 1);
 
@@ -191,13 +191,13 @@ INSERT INTO `sadaf`.`Books`
 VALUES
 ("888",
 "",
-"/BookReads/asset/books/pdf/8.pdf",
+"./UploadFile/pdf/8.pdf",
 "To Kill a Mockingbird is a novel by the American author Harper Lee. It was published in 1960 and, instantly successful in the United States, it is widely read in high schools and middle schools. To Kill a Mockingbird has become a classic of modern American literature, winning the Pulitzer Prize.
 ",
 257,
 "To Kill a Mockingbird",
 NULL,
-"/BookReads/asset/books/img/8_cover.jpeg",
+"./UploadFile/img/8_cover.jpeg",
 "Harper Lee",
 1);
 
@@ -217,12 +217,14 @@ INSERT INTO `sadaf`.`Books`
 VALUES
 ("999",
 "",
-"/BookReads/asset/books/pdf/9.pdf",
+"./UploadFile/pdf/9.pdf",
 "Le Morte d'Arthur tells the story of King Arthur and his Knights at the Round Table. Arthur, who is son of King Uther Pendragon but was raised by another family, takes his rightful place as king when, as a boy, he is able to pull the sword called Excalibur from the stone.
 ",
 150,
 "The Legends of King Arthur and His Knights",
 NULL,
-"/BookReads/asset/books/img/9_cover.jpeg",
+"./UploadFile/img/9_cover.jpeg",
 "James Thomas Knowles",
 1);
+
+

b3836d5298359f8e9f80db9227b484ba3039d9a4
kasra korminejad <kasra76@gmail.com>
2021-02-08 10:32:48 +0330

    search sending GET request issue fixed

--- www/sadaf/searchBook.php
+++ www/sadaf/searchBook.php
@@ -140,7 +140,7 @@
 
             echo "<hr>";
             for($i = 0; $i < $count; $i++){
-                echo "<div class='row'><div class='col-4'><a href=./bookProfile.php?ّISBN=".$rec['ISBN']." style='text-align: center;'>".$title[$i]."</a></div>";
+                echo "<div class='row'><div class='col-4'><a href='./bookProfile.php?ISBN=".$rec['ISBN']."' style='text-align: center;'>".$title[$i]."</a></div>";
                 echo "<div class='col-4'><img src='".$img_path[$i]."' style='width: 12vw;'></div></div><hr>";
             }
         ?>
